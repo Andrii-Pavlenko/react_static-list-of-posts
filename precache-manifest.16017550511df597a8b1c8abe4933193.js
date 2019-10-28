0035912c971da6a3c936dab9359965da4f2d3a0d
Andrii <farikpite@gmail.com>
2019-10-28 14:23:36 +0200

    Updates

---
+++ precache-manifest.16017550511df597a8b1c8abe4933193.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec688924b6ce22bf7378f1c4837e4c80",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "5f7242ed8918f2ae5d74",
+    "url": "/react_static-list-of-posts/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "26050395c0748c903b05",
+    "url": "/react_static-list-of-posts/static/js/2.67a0c2f2.chunk.js"
+  },
+  {
+    "revision": "5f7242ed8918f2ae5d74",
+    "url": "/react_static-list-of-posts/static/js/main.c5397691.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
